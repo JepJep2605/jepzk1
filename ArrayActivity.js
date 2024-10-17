8b0d52e38bf36d20dc0f70c694449394c09655cb
JEFFREY R. RAGIL <jeffreyragil6@gmail.com>
2024-10-17 07:54:32 +0000

    initial commit

---
+++ ArrayActivity.js
@@ -0,0 +1,14 @@
+
+let favSingers = ["Adele", "Ed Sheeran", "Bruno Mars"];
+
+
+console.log(favSingers[0]);
+
+let favNumbers = [7, 11, 23, 42];
+
+let mixedArr = ["string", ["otherarray"], 123, true];
+
+console.log(mixedArr[0]);
+console.log(mixedArr[1]); 
+console.log(mixedArr[2]); 
+console.log(mixedArr[3]); 
